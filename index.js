63fcde53b75e51a8a31348542b1abc71b8cce806
dhkdtld37 <149128094+dhkdtld37@users.noreply.github.com>
2024-01-25 15:01:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,11 @@ console.log(`
 `);
 
 const args = process.argv.slice(2);
-console.log("발표자:"+args[Math.floor(Math.random()*5)]);
+const randomIndex = Math.floor(Math.random() * args.length);
+
+if (args.length > 0) {
+    console.log("발표자: " + args[randomIndex]);
+} else {
+    console.log("사용법: node random-name.js 홍길동 임꺽정 이순신");
+    process.exit(1);
+}
