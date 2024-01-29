c093df5c9a58becff4264999d09a3128559d94e3
dhkdtld37 <hyeonls96@naver.com>
2024-01-29 12:41:54 +0900

    JS Default, Rest Parameters for RF
    Fix index.js

--- index.js
+++ index.js
@@ -20,24 +20,23 @@ console.log(`
 `);
 
 class People {
-	constructor(peoples){
-		this.peoples = peoples;
-	}
+  constructor(peoples = []) {
+    this.peoples = peoples;
+  }
 
-	select() {
-		const randomName = names [Math.floor(Math.random() * names.length)];
- 		console.log(`발표자 : ${randomName}`); 
-	}
+  select() {
+    const randomName = this.peoples[Math.floor(Math.random() * this.peoples.length)];
+    console.log(`발표자 : ${randomName}`);
+  }
 }
 
-const argv = process.argv;	// 입력
-const names = argv.slice(2);	// argv 배열에서 인덱스 2부터 끝까지의 부분 배열을 추출
+const [, , ...names] = process.argv; // Rest Parameter를 사용하여 필요한 부분을 추출
 
 if (names.length < 3) {
   console.log("최소 3명 이상의 발표자를 입력하시오");
   process.exit();
 }
 
-var people = new People(names);
+const people = new People(names);
 people.select();
 
